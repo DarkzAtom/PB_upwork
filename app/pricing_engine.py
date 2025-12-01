95270fdab7d4ebcede4f859dbe51f1e5f06d1d48
M F <rekrut.fedosenko@gmail.com>
2025-12-01 15:49:03 +0100

    ADD: method for getting applicable pricing rule to engine

--- app/pricing_engine.py
+++ app/pricing_engine.py
@@ -1,5 +1,8 @@
-from db_routers_connection import SessionLocal
-from fx_rates.fx_rates_model import FxRate
+from sqlalchemy import or_
+from app.db_routers_connection import SessionLocal
+from app.fx_rates.fx_rates_model import FxRate
+from app.pricing_rules.pricing_rules_model import PricingRule
+from app.supplier_price.supplier_price_model import SupplierPrice
 from decimal import Decimal
 from sqlalchemy.orm import Session
 
@@ -15,17 +18,28 @@ class PricingEngine:
     #     supplier_prices.order_by(cost_with_logistics ASC, lead_time_days ASC)
     #     return supplier_price[0]
 
-    def get_pricing_rule(self, supplier_price):
-        pass
-    #     pricing_rules = db.get_pricing_rules(supplier_price.supplier_id, supplier_price.part.brand_id, supplier_price.part.category_id, supplier_price.price, supplier_price.warehouse_id)
-    #     if pricing_rules == null
-    #     return generic_pricing_rule
-    #     pricing_rules.order_by(priority ASC)
-    #     return pricing_rules[0]
-
     def convert_to_pln(self, price: Decimal, currency: str) -> Decimal:
         exchange_rate = self.db.query(FxRate).filter(FxRate.from_currency == currency, FxRate.to_currency == 'PLN').first()
         return price * exchange_rate.rate
+
+    def get_pricing_rule(self, supplier_price: SupplierPrice) -> PricingRule:
+        price_pln = self.convert_to_pln(supplier_price.base_price, supplier_price.currency)
+        pricing_rule = self.db.query(PricingRule).order_by(PricingRule.priority.asc()).filter(
+            or_(PricingRule.supplier_id == supplier_price.supplier_id, PricingRule.supplier_id.is_(None)),
+            or_(PricingRule.brand_id == supplier_price.part.brand_id, PricingRule.brand_id.is_(None)),
+            or_(PricingRule.category_id == supplier_price.part.category_id, PricingRule.category_id.is_(None)),
+            PricingRule.price_min < price_pln,
+            PricingRule.price_max >= price_pln,
+            PricingRule.warehouse_region == supplier_price.warehouse.region,
+            PricingRule.is_active).first()
+        # If no rules applicable - use default rule
+        return pricing_rule or PricingRule(
+            rule_name='Default margin rule',
+            margin_percent=Decimal('10'),
+            fixed_markup=Decimal('10'),
+            rounding_rule='Ceiling',
+            priority=0,
+            is_active=True)
 
     def calculate_price_with_logistics(self, supplier_price):
         pass
@@ -39,9 +53,19 @@ class PricingEngine:
     #     final_price = price_with_logistics * (100% + pricing_rule.margin_percent) + pricing_rule.fixed_markup
     #     return final_price
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = SessionLocal()
     eng = PricingEngine(db)
 
     value = eng.convert_to_pln(5, 'EUR')
     print(value)
+
+    supplier_price = db.query(SupplierPrice).first()
+    print(supplier_price.currency)
+    print(supplier_price.base_price)
+    print(supplier_price.warehouse.region)
+
+    pricing_rule = eng.get_pricing_rule(supplier_price)
+    print(pricing_rule.rule_name)
+    print(pricing_rule.margin_percent)
+    print(pricing_rule.fixed_markup)
